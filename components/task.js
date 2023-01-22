db140e655e0cf140408d43fd405f94b94fb23207
Onu Rares-Andrei <onuraresandrei57@gmail.com>
2023-01-22 11:53:02 +0000

    make button larger

--- components/task.js
+++ components/task.js
@@ -1,17 +1,17 @@
 export default function Task() {
+    const checked = true
     return (
         <div className="flex flex-row rounded-lg shadow-md h-[60px] items-center mx-10">
-            <div className="ml-5">
+            <div className="pl-5 pr-5 pt-4 pb-3">
             <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="8" cy="8" r="7.5" fill="#FAFAFA" stroke="#B3B3B3" />
             </svg>
             </div>
-            <div className="ml-3">
+            <div>
                 <h1 className="font-thin">
                     Task Test, idk
                 </h1>
             </div>
-
         </div>
     )
 }
