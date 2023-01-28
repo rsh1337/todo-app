5d3261d9b9e775f0a230b72a78e23d9417c99581
Onu Rares-Andrei <onuraresandrei57@gmail.com>
2023-01-28 17:28:35 +0000

    adding support for desktop

--- components/header.js
+++ components/header.js
@@ -2,9 +2,9 @@ import Image from "next/image"
 
 export default function Header() {
     return (
-        <div className="flex rounded-b-lg shadow-md h-16 items-center justify-between bg-[#FFFFFF]">
-            <h1 className="ml-4 font-medium">Welcome</h1>
-            <Image className="mr-5" src={"/icon.png"} alt="pfp" width={40} height={40}/>
+        <div className="flex rounded-b-lg shadow-md h-16 items-center justify-between md:justify-end md:flex-row-reverse bg-[#FFFFFF]">
+            <h1 className="ml-10 md:ml-1 font-medium">ToDo</h1>
+            <Image className="mr-10 md:ml-10 md:mr-2" src={"/icon.png"} alt="pfp" width={40} height={40}/>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,6 @@ import { useEffect, useState } from "react";
 import Header from "../components/header";
 import Task from "../components/task";
 import {
-  Modal,
-  ModalBody,
-  ModalContent,
-  ModalOverlay,
-  useDisclosure,
   PopoverTrigger,
   PopoverContent,
   Popover,
@@ -17,7 +12,7 @@ export default function Home() {
   const [tasks, setTasks] = useState([]);
   const [newTaskName, setNewTaskName] = useState("");
   const [newTaskDetails, setNewTaskDetails] = useState("");
-  const [editingTask, setEditingTask] = useState(-1);
+
   useEffect(() => {
     if (typeof window !== "undefined" && window.localStorage) {
       const storedTasks = JSON.parse(localStorage.getItem("tasks"));
@@ -27,6 +22,7 @@ export default function Home() {
       }
     }
   }, []);
+
   useEffect(() => {
     if (typeof window !== "undefined" && window.localStorage) {
       localStorage.setItem("tasks", JSON.stringify(tasks));
@@ -44,11 +40,13 @@ export default function Home() {
     setNewTaskName("");
     setNewTaskDetails("");
   };
+
   const handleRemove = (index) => {
     const newTasks = [...tasks];
     newTasks.splice(index, 1);
     setTasks(newTasks);
   };
+
   const handleCheck = (index) => {
     const updatedTasks = tasks.map((task, i) => {
       if (i === index) {
@@ -59,20 +57,52 @@ export default function Home() {
     });
     setTasks(updatedTasks);
   };
+
   const handleDeleteChecked = () => {
     const updatedTasks = tasks.filter((task) => !task.checked);
     setTasks(updatedTasks);
     localStorage.setItem("tasks", JSON.stringify(updatedTasks));
   };
-  const handleEdit = (index) => {
-    setEditingTaskIndex(index);
-  }
+
   return (
-    <div className="bg-[#FCFCFC] h-screen w-screen">
+    <div className="bg-[#FCFCFC] h-screen w-screen md: w-full">
       <div className="top-0 sticky">
         <Header />
       </div>
-      <div className="fixed bottom-0 right-0">
+      <div className="hidden md:flex flex-row ml-10 mt-5 gap-4">
+        <CreateTaskModal handleSubmit_create={handleSubmit} newTaskName_create={newTaskName} newTaskDetails_create={newTaskDetails} setNewTaskDetails_create={setNewTaskDetails} setNewTaskName_create={setNewTaskName} />
+        <button
+          className="bg-[#F6F6F6] rounded-lg p-2 flex flex-row items-center gap-2 text-sm"
+          onClick={handleDeleteChecked}
+        >
+          <svg
+            width="25"
+            height="25"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M14 16.16L10.04 12.2M13.96 12.24L10 16.2M10 6H14C16 6 16 5 16 4C16 2 15 2 14 2H10C9 2 8 2 8 4C8 6 9 6 10 6Z"
+              stroke="black"
+              strokeWidth="1.5"
+              strokeMiterlimit="10"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              d="M16 4.02C19.33 4.2 21 5.43 21 10V16C21 20 20 22 15 22H9C4 22 3 20 3 16V10C3 5.44 4.67 4.2 8 4.02"
+              stroke="black"
+              strokeWidth="1.5"
+              strokeMiterlimit="10"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+          Delete Checked
+        </button>
+      </div>
+      <div className="fixed bottom-0 right-0 md:hidden">
         <Popover>
           {({ isOpen }) => (
             <>
@@ -151,13 +181,13 @@ export default function Home() {
                     />
                   </svg>
                 </button>
-                <CreateTaskModal handleSubmit_create={handleSubmit} newTaskName_create={newTaskName} newTaskDetails_create={newTaskDetails} setNewTaskDetails_create={setNewTaskDetails} setNewTaskName_create={setNewTaskName}/>
+                <CreateTaskModal handleSubmit_create={handleSubmit} newTaskName_create={newTaskName} newTaskDetails_create={newTaskDetails} setNewTaskDetails_create={setNewTaskDetails} setNewTaskName_create={setNewTaskName} />
               </PopoverContent>
             </>
           )}
         </Popover>
       </div>
-      <div className="mb-10">
+      <div className="md:grid md:grid-cols-2 lg:grid-cols-3 gap-3">
         {tasks.map((task, index) => (
           <div className="mt-5 mb-10" key={index}>
             <Task
@@ -165,7 +195,6 @@ export default function Home() {
               details={task.details}
               deletetask={() => handleRemove(index)}
               checked={task.checked}
-              setEditingTask={setEditingTask}
               ischecked={() => handleCheck(index)}
             />
           </div>
