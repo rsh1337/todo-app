50e952f4ee60a0de1dced8edcca4ab3578f844a6
Onu Rares-Andrei <onuraresandrei57@gmail.com>
2023-01-31 14:28:57 +0000

    pwa prompt for ios

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
+import loadable from '@loadable/component';
+const PWAPrompt = loadable(() => import('react-ios-pwa-prompt'));
 
 export default function App({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Component {...pageProps} />
+      <PWAPrompt
+					copyTitle="Add to homescreen"
+					copyBody="This website has functionality like an app. Add it to your home screen for an even more immersive experience!"
+					copyShareButtonLabel="Press the 'Share' button."
+					copyAddHomeButtonLabel="Press the 'Add to homescreen' button."
+					copyClosePrompt="Close"
+				/>
     </ChakraProvider>
   )
 }
