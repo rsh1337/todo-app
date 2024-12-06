d379634cc2e378ef8471636a27227b2b01bb2610
Onu Rares-Andrei <onuraresandrei57@gmail.com>
2024-12-06 13:59:26 +0000

    Update

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,10 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import '../styles/globals.css'
-import loadable from '@loadable/component';
-const PWAPrompt = loadable(() => import('react-ios-pwa-prompt'));
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-      <PWAPrompt
-					copyTitle="Add to HomeScreen"
-					copyBody="This app can also be used offline. Add it to your home screen for a more immersive experience!"
-					copyShareButtonLabel="Press the 'Share' button."
-					copyAddHomeButtonLabel="Press the 'Add to homescreen' button."
-					copyClosePrompt="Close"
-				/>
-    </ChakraProvider>
-  )
+    return (
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 }
-
